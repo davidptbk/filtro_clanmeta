51dd066d5316496b65e31f3fce451dcbc493d7c6
DavidPT <davidpt94@gmail.com>
2024-06-04 12:26:49 -0500

    inciailization project

---
+++ src/main/java/com/riwi/Filtro/de/clan/meta/FiltroDeClanMetaApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.Filtro.de.clan.meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltroDeClanMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltroDeClanMetaApplication.class, args);
+	}
+
+}
